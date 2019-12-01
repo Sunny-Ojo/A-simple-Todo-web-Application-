c3832d2d12906bbe7bbdd90a41c89bebbda4b30a
Sunny-Ojo <sunny.ojo.14@gmail.com>
2019-12-01 15:35:48 +0100

    made another change to the index page

--- index.php
+++ index.php
@@ -20,47 +20,46 @@ if (isset($_POST["submit"])) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <meta
-      name="description"
-      content="A simple to do web application that allows users to add, edit and delete
-       their todo lists"
-    />
+    <meta name="description" content="A simple to do web application that allows users to add, edit and delete
+       their todo lists" />
     <meta name="author" content="Sunny Ojo" />
     <link rel="stylesheet" href="assets/css/index.css" />
     <link rel="stylesheet" href="assets/css/all.css" />
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
     <link rel="stylesheet" href="assets/css/fontawesome.min.css" />
     <title>To do Web Application</title>
-  </head>
-  <body>
+</head>
+
+<body>
     <div class="container">
-      <div class="main col-md-10 offset-md-1">
-        <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="post">
-        <h2 class="text-center">Create your Todo Lists</h2>
-<?php if (isset($error)) {echo $error;}
+        <div class="main col-md-10 offset-md-1">
+            <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="post">
+                <h2 class="text-center">Create your Todo Lists</h2>
+                <?php if (isset($error)) {echo $error;}
 if (isset($success)) {echo $success;}
 ?>
-        <input type="text" name="todo" class="form-control" /> <br />
-        <button type="submit" name="submit" class="btn btn-success">
-          Create
-        </button>
-        </form>
+                <input type="text" name="todo" class="form-control" /> <br />
+                <button type="submit" name="submit" class="btn btn-success">
+                    Create
+                </button>
+            </form>
 
-      </div>
-      <br>
-      <div>
-      <table class=" details table-hover" border=1 align=center >
-          <tr>
-            <th>ID</th>
-            <th>TODO TASKS</th>
-            <th>DATE CREATED</th>
-            <th>ACTION</th>
-          </tr>
-          <?php
+        </div>
+        <br>
+        <div>
+            <table class=" details table-hover" border=1 align=center>
+                <tr>
+                    <th>ID</th>
+                    <th>TODO TASKS</th>
+                    <th>DATE CREATED</th>
+                    <th>ACTION</th>
+                </tr>
+                <?php
 $dis = $conn->query("SELECT * FROM `tasks`");
 foreach ($dis as $me) {
     echo "<tr>
@@ -75,8 +74,9 @@ foreach ($dis as $me) {
 }
 
 ?>
-        </table>
-      </div>
+            </table>
+        </div>
     </div>
-  </body>
+</body>
+
 </html>
